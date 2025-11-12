e6526caa656ed6dc565555aa386d04653834d231
Xuanhe Liu <liuxh25@mails.tsinghua.edu.cn>
2025-11-12 08:13:10 +0800

    Update analyzer.lisp

--- analyzer.lisp
+++ analyzer.lisp
@@ -4,36 +4,36 @@
 
 (in-package :analyzer)
 
-(defvar *know-or-not* t
-  "全局变量，表示是否知道答案。")
+(defvar *ana-answer-file* "analyzer-answer.txt")
+(defvar *ana-config-file* "analyzer-config.txt")
+(defvar *know-or-not* t)
+(defvar *ana-answer* (make-hash-table :test 'equal))
+(defvar *analyzers* (make-hash-table :test 'equal))
 
-(defvar *ana-answer* (make-hash-table :test 'equal)
-  "全局哈希表，存储已知问题及对应答案。")
-
-;;; 初始化函数，读取答案文件并写入哈希表
 (defun ana-init (filename)
-  (with-open-file (stream filename :direction :input :if-does-not-exist :create)
-    (loop for line = (read-line stream nil)
-          while line
-          do (let ((parts (split-sequence #\| line)))
-               (when (= (length parts) 2)
-                 (setf (gethash (first parts) *ana-answer*) (second parts)))))))
+  (when (probe-file filename)
+    (with-open-file (stream filename :direction :input)
+      (loop for line = (read-line stream nil)
+            while line
+            do (let ((parts (split-sequence #\| line)))
+                 (when (= (length parts) 2)
+                   (setf (gethash (first parts) *ana-answer*)
+                         (second parts))))))))
 
-;;; 向文件追加主题和解释
 (defun append-role-to-file (filename topic explain)
   (with-open-file (out filename
                       :direction :output
                       :if-exists :append
                       :if-does-not-exist :create)
-    (format out "~a|~a~%" topic explain)))
+    (format out "~a|~a~%" topic explain))
+  (values topic explain))
 
-;;; 检查str是否有答案，没有则追加并返回默认回复
 (defun check (str)
   (let ((answer (gethash str *ana-answer*)))
     (if answer
         answer
         (progn
-          (append-role-to-file "analyzer-answer.txt" str "不清楚")
+          (append-role-to-file *ana-answer-file* str "不清楚")
           "什么我不是很清楚，这是一个暂未记录的奇怪问题..."))))
 
 (defclass analyzer ()
@@ -42,6 +42,15 @@
    (price :initarg :price :accessor ana-price :initform 100)
    (tht :initarg :tht :accessor ana-tht :initform "中立")))
 
+(defun init-analyzer (filename)
+  (when (probe-file filename)
+    (with-open-file (stream filename :direction :input)
+      (loop for line = (read-line stream nil)
+            while line
+            do (let ((parts (split-sequence #\| line)))
+                 (when (= (length parts) 5)
+                   (destructuring-bind (a b c d e) parts
+                     (setf (gethash a *analyzers*) (list b c d e)))))))))
 (defun main-analyze (str whoana)
   (format t "你好！我是~a!~%" (ana-name whoana))
   (sleep 0.5)
@@ -54,19 +63,28 @@
   (let ((result (check str)))
     (if (not (string= result "什么我不是很清楚，这是一个暂未记录的奇怪问题..."))
         (progn
-          (format t "从~a的角度分析，你的问题主要关于~a~%" (ana-tht whoana) result)
-          (sleep 0.5)
+          (format t "以~a的态度分析，你的问题主要关于~a~%" (ana-tht whoana) result)
           (format t "价格：~a~%" (ana-price whoana)))
         (progn
           (format t "关于这个问题，这是：~a~%" result)
-          (format t "你能告诉我一些关于这个问题的答案吗？~%")
+          (format t "请输入你的答案： ")
           (let ((user-answer (read-line)))
-            (append-role-to-file "analyzer-answer.txt" str user-answer)
+            (append-role-to-file *ana-answer-file* str user-answer)
             (setf (gethash str *ana-answer*) user-answer)
             (format t "感谢您的教导！我又学会了新的知识~%")
             (setf *know-or-not* nil))))))
 
-;; 对外接口示例（如需在 REPL 交互，可使用以下典型流程）：
-;; (ana-init "analyzer-answer.txt")
-;; (let ((a (make-instance 'analyzer)))
-;;   (main-analyze "xxx问题" a))
+(defun analyzer-instance (analyzer-list)
+  (maphash
+   (lambda (k v)
+     (let ((sym (intern (format nil "ANALYZER-~A" k))))
+       (set sym
+            (make-instance 'analyzer
+              :name (first v)
+              :rspon (second v)
+              :price (third v)
+              :tht (fourth v)))))
+   analyzer-list))
+(defun init-all ()
+  (ana-init *ana-config-file*)
+  (init-analyzer *ana-answer-file*))
